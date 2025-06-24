a221a13aa0fa6a3a92e245c018f81463e2c1f8b8
Hampus Farner <hampusfarner@outlook.com>
2025-06-24 05:49:31 +0200

    Update prisma

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import config from './config/config.js';
+import routes from './routes/routes.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
-app.use('/', (req, res) => res.status(200).send('HEALTHY'));
+app.use('/auth', routes.auth);
 
 app.listen(config.port, () =>
   console.log(`Listening in on port ${config.port}`),
